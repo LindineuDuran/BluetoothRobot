41be4b9c680453569c1fa35af74c9a1f5d7f4ab0
LindineuDuran <37485332+LindineuDuran@users.noreply.github.com>
2018-11-08 11:53:14 -0200

    Atualização do Sketch no Diagrama
    Recarregado o sketche para dentro do diagrama

--- BluetoothRobot.ino
+++ BluetoothRobot.ino
@@ -80,7 +80,7 @@ int duracaodasnotas[] = {100, 100, 100, 100, 100, 100, 100};
 unsigned long previousMillis = 0; // will store last time LED was updated
 
 // constants won't change :
-const long interval = 300; // interval at which to blink (milliseconds)
+const long interval = 20; // interval at which to blink (milliseconds)
 
 
 void setup()
